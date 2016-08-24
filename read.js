fca1186056f6fa4591ab33f010fc080e8bf78e9f
atungare <ajay.tungare@gmail.com>
2016-08-24 16:24:33 -0400

    working session progress

--- read.js
+++ read.js
@@ -6,3 +6,31 @@ console.log(`Result:
     "beep": "beep"
   }
 order of properties is not important **`);
+
+import fs from 'fs';
+
+function fsReadPromise (filename) {
+  return new Promise ( (resolve, reject) => {
+    fs.readFile(filename, 'utf-8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    })
+  });
+}
+
+function readWrite () {
+  const mocks = ['./mocks/beep.json', './mocks/bleep.json', './mocks/bloop.json']
+
+  const promises = mocks.map(fsReadPromise);
+
+  Promise.all(promises)
+    .then((results) => {
+      // Parse each result and merge them into a new object
+    })
+    .then(() => {
+      // Then, write the new object as json to the specified directory
+    })
+
+}
+
+
